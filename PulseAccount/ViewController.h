d662a86d1ad5bfd513701f8c952415a8ee147f34
Alivelu Ravula <alivelu7193@achiever-25.local>
2018-01-20 10:27:17 +0530

    Initial Commit

---
+++ PulseAccount/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PulseAccount
+//
+//  Created by Alivelu Ravula on 1/20/18.
+//  Copyright © 2018 Alivelu Ravula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PulseAccount/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PulseAccount
+//
+//  Created by Alivelu Ravula on 1/20/18.
+//  Copyright © 2018 Alivelu Ravula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
